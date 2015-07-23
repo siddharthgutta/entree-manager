cdade8d22bf436fb236af9422366b2127d632905
Faraz <pmh@KKHs-Mac-mini.local>
2015-07-23 13:22:53 +1000

    Initial Commit

---
+++ EntreeManage/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  EntreeManage
+//
+//  Created by Faraz on 7/23/15.
+//  Copyright (c) 2015 Faraz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ EntreeManage/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  EntreeManage
+//
+//  Created by Faraz on 7/23/15.
+//  Copyright (c) 2015 Faraz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
